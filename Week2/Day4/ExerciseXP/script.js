e175de5dffa955dfddd608fff0cb363fae7bc827
Javid1996 <alcavid96@gmail.com>
2023-09-15 01:52:03 +0400

    ExerciseXPDaily

--- Week2/Day4/ExerciseXP/script.js
+++ Week2/Day4/ExerciseXP/script.js
@@ -67,16 +67,18 @@ function calculateTip(){
     
     let bill=prompt("Please add amount of the bill");
     let tip;
-    if(!isNaN(bill)){
-        if(bill<50){
-            tip=bill*0.2;
-            console.log(`Tip: ${tip} ; Final bill: ${tip+bill}`)
-        }else if(bill>=50 && bill<=200){
-            tip=bill*0.15;
-            console.log(`Tip: ${tip} ; Final bill: ${tip+bill}`)
-        }else if(bill>200){
-            tip=bill*0.1;
-            console.log(`Tip: ${tip} ; Final bill: ${tip+bill}`)
+    let newNumber=Number(bill);
+    parseInt(newNumber);
+    if(!isNaN(newNumber)){
+        if(newNumber<50){
+            tip=newNumber*0.2;
+            console.log(`Tip: ${tip} ; Final bill: ${tip+newNumber}`)
+        }else if(newNumber>=50 && newNumber<=200){
+            tip=newNumber*0.15;
+            console.log(`Tip: ${tip} ; Final bill: ${tip+newNumber}`)
+        }else if(newNumber>200){
+            tip=newNumber*0.1;
+            console.log(`Tip: ${tip} ; Final bill: ${tip+newNumber}`)
         }
     }else{
         console.log("you add not a number")
@@ -100,13 +102,153 @@ calculateTip();
 // At the end, console.log the sum of all numbers that are divisible by 23.
 
 
+
+// let container=0;
 // function isDivisible(){
 
 //     for(let i =0; i<=500;i++){
-//         let container=0
-//         whyle(i%23==0){
+        
+//         if(i%23==0){
 //             container+=i;
+//             console.log(i)
 //         }
 
 //     }
+//     return  container;
+// }
+
+// console.log(`the sum of all numbers are ${isDivisible()}`);
+
+
+
+
+
+
+
+
+// 🌟 Exercise 4 : Shopping List
+
+
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+} 
+
+
+
+// Add the stock and prices objects to your js file.
+
+// Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”. It means that you have 1 banana, 1 orange and 1 apple in your cart.
+
+// Create a function called myBill() that takes no parameters. 
+
+// The function should return the total price of your shoppingList. In order to do this you must follow these rules:
+// The item must be in stock. (Hint : check out if .. in)
+// If the item is in stock find out the price in the prices object.
+
+// Call the myBill() function.
+// xxxxxxxxxxxxxxxxxxxxx
+
+// let shoppingList=["banana","orange", "apple"];
+
+// function myBill(){
+    
+//     for(let i of shoppingList){
+//         console.log(i);
+//         if(stock[i]>0){
+//             console.log(`${i} : ${prices[i]} `)
+//         }else{
+//             console.log(`no ${i}`)
+//         }
+//     }
+// }
+
+// myBill();
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+
+// Exercise 5 : What’s In My Wallet ?
+let change=[0.25,0.1,0.05,0.01];
+let sum=0;
+function changeEnough(itemPrice, amountOfChange){
+    for(let i = 0 ; i<change.length;i++){
+    
+        sum+=change[i]*amountOfChange[i]
+        console.log(sum)
+    }
+
+    if(sum>itemPrice){
+        return true;
+        
+    }else{
+        return false;
+    }
+}
+
+console.log(changeEnough(6.25, [5,10,12,0]));
+
+
+
+
+
+//  Exercise 6 : Vacations Costs
+
+
+// function hotelCost(){
+//    let days = prompt("how many nights you would like to stay in a hotel?");   
+//    let numDays=Number(days); 
+//    let totalAmount=0;
+//    if(!isNaN(days)){
+//     totalAmount= numDays*140;
+//     return totalAmount;
+//    }else{
+//     days=prompt("how many nights you would like to stay in a hotel???")
+//    }
+// }
+// console.log(hotelCost());
+
+
+// function planeRideCost(){
+//     let userDestination=""
+//      userDestination = prompt("Please add your destination?");
+
+//     if(userDestination===String){
+//         if(userDestination=="London"){
+//             return "London:"+" "+ 183 +'$' 
+//         }else if(userDestination=="Paris"){
+//             return "Paris:"+" "+ 220 +'$'
+//         }else{
+//             return "All other destination :" + " " + 300 +'$' 
+//         }
+//     }else{
+//         // while(userDestination!==String){
+//         //     let userDestination = prompt("Please add your destination??");
+//         }
+//     }
+// }
+// console.log(planeRideCost());
+
+
+// Define a function called rentalCarCost().
+// It should ask the user for the number of days they would like to rent the car.
+// If the user doesn’t answer or if the answer is not a number, ask again.
+// Calculate the cost to rent the car. The car costs $40 everyday.
+// If the user rents a car for more than 10 days, they get a 5% discount.
+// The function should return the total price of the car rental. 
+
+// function rentalCarCost(){
+
 // }
