3a92e4d6c4db6a637789ea1edfa679e1f09218a8
nergız seyidova <nergisseyidova@gmail.com>
2023-10-02 11:44:43 +0400

    checker comments

--- Week4 /Day5/Dailychallenge/script.js
+++ Week4 /Day5/Dailychallenge/script.js
@@ -24,3 +24,14 @@ getAnagram('str1','str2')
 // "Astronomer" is an anagram of "Moon starer"
 // "School master" is an anagram of "The classroom"
 // "The Morse Code" is an anagram of "Here come dots"
+
+// !!! TASK REDO
+
+// !!! steps
+/* 
+1. stringlerden space silmek 
+2. stirngleri arraye cevirmek
+3. stringlerin lengthlerini muqayise etmek eger lengthler beraber deyilse demek sozler eyni deyil
+4. stringleri loop getmek her bir elementi muqayise etmek
+5. her biri birbirine beraberdise demekki eyni herfler istifade olunub
+*/
