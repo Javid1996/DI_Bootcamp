6a466d81ecb09773ed1054a51917b3218349e0f8
nergız seyidova <nergisseyidova@gmail.com>
2023-10-12 11:39:33 +0400

    checker comments

--- Week6/Day3/XP1/Без названия.sql
+++ Week6/Day3/XP1/Без названия.sql
@@ -155,6 +155,53 @@ WHERE return_date IS NULL
 ORDER BY rental_rate  DESC LIMIT 30;
 
 
+-- !!! solution
+-- 1.
+select film.title
+from (actor join film_actor on actor.actor_id = film_actor.actor_id)
+join film on film.film_id = film_actor.film_id
+where film.description like '%Sumo%' 
+and actor.first_name = 'Penelope' 
+and actor.last_name = 'Monroe'
+
+-- 2.
+SELECT title FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+WHERE category_id = 6
+AND length < 60
+AND rating = 'R';
+
+-- 3.
+SELECT film.title 
+FROM film
+ON film.film_id = inventory.film_id
+JOIN rental
+ON rental.inventory_id = inventory.inventory_id
+JOIN customer
+ON rental.customer_id = customer.customer_id
+WHERE first_name = 'Matthew'
+AND last_name = 'Mahan'
+AND rental_rate > 4
+AND rental.return_date BETWEEN '28/07/2005' AND '01/08/2005'
+
+-- 4.
+SELECT film.title, film.description, film.replacement_cost
+FROM film
+JOIN inventory
+ON film.film_id = inventory.film_id
+JOIN rental
+ON rental.inventory_id = inventory.inventory_id
+JOIN customer
+ON rental.customer_id = customer.customer_id
+where customer.first_name = 'Matthew'
+and customer.last_name = 'Mahan'
+and film.title like '%Boat%' 
+or film.description like '%Boat%'
+order by film.replacement_cost desc
+limit 1
+-- !!!
+
+
 
 
 
