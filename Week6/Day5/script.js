55881dea491ed9e8cc1c135d1f14ca59e494d9e0
Javid1996 <alcavid96@gmail.com>
2023-10-13 11:13:19 +0400

    W6D4

--- Week6/Day5/script.js
+++ Week6/Day5/script.js
@@ -1,8 +1,7 @@
 let form  = document.getElementById('task_form')
 form.addEventListener('submit', function() {
-    // Get the value of the name field.
-    // var name = document.getElementById('name').value;
+   
     let task_value= stringify.json(form.vale)
-    // Save the name in localStorage.
+    
     localStorage.setItem('task', task_value);
   });
