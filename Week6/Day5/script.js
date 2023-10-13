b2ea6a2fe5177a78bea61e04dbaf35a23d75824b
Javid1996 <alcavid96@gmail.com>
2023-10-13 11:24:31 +0400

    W6D4V2

--- Week6/Day5/script.js
+++ Week6/Day5/script.js
@@ -1,7 +1,15 @@
-let form  = document.getElementById('task_form')
-form.addEventListener('submit', function() {
+// let name = document.getElementById('name')
+// let description = document.getElementById('description')
+// let name = document.getElementById('')
+let form_input = document.getElementsByClassName("form_input")
+let form = 
+
+
+// let form  = document.getElementById('task_form')
+// form.addEventListener('submit', function() {
    
-    let task_value= stringify.json(form.vale)
+//     let task_value= stringify.json(form.vale)
     
-    localStorage.setItem('task', task_value);
-  });
+//     localStorage.setItem('task', task_value);
+//   });
+
