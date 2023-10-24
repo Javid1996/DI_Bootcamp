2099d0361ec5a8295ad7536742ff31e25718cce9
Javid1996 <alcavid96@gmail.com>
2023-10-24 23:52:56 +0400

    w7d1

--- Week7/Day1/XP/task4/todoApp/todo.js
+++ Week7/Day1/XP/task4/todoApp/todo.js
@@ -1,11 +1,30 @@
 class todoList {
-    constructor(){
+    constructor(a,b,c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
         const tasks=[]
     }
-    method1()
-    method2()
-    method3()
+    method1(){
+        tasks.push(this.a,this.b,this.c)
+        console.log(tasks)
+    }
+    method2(){
+        const filteredTasks = tasks.map((item) => {
+            if (item===false) {
+              return item===true;
+            }console.log(tasks);
+
+          });
+    }
+    method3(){
+        tasks.forEach(task => 
+            console.log(task)
+        );
+    }
 }
+
+
 
 
 
